b25b877c25c8325f28a85547a18d3064cf65d377
Propeller-85 <propeller85@mail.ru>
2022-09-01 19:57:29 +0300

    hh

--- game.py
+++ game.py
@@ -3,7 +3,7 @@ import numpy as np
 number = np.random.randint(1, 101) # загадываем число
 count = 0
 
-while True: #58
+while True: #587
     count += 1
     predict_number = int(input("Угадай число от 1 до 100"))
     
