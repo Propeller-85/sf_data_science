4294023d0c4530e6ea8749fe102fa494fe05ee76
Propeller-85 <propeller85@mail.ru>
2022-10-30 20:30:37 +0200

    Git

--- progect_0/experiment.py
+++
@@ -1,9 +0,0 @@
-import numpy as np
-
-#arr = np.linspace(-6, 21, 60, retstep=True, endpoint=False)
-
-#print(arr)
-
-simplelist = [19, 242, 14, 152, 142, 1000]
-n = sum(simplelist) / len(simplelist)
-print(n)
